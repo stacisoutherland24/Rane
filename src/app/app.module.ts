d2f341dd88de3b87fdea729896065b0ab04c1ce3
Staci Southerland <stacisoutherland24@gmail.com>
2023-07-08 16:51:43 -0500

    made changes to the navbar not responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ButtonModule } from 'primeng/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,19 +11,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'about-me', component: AboutMeComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, AboutMeComponent],
+  declarations: [AppComponent, AboutMeComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ButtonModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
-    RouterModule.forRoot([{ path: 'about-me', component: AboutMeComponent }]),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
