868854f75666ca6406223f55bd7f928e838fe7ca
Staci Southerland <stacisoutherland24@gmail.com>
2023-07-11 10:49:23 -0500

    Before Lash Cove changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  title = 'Rane Beauty Studio';
+  goToSquarespace() {
+    window.location.href = 'https://lashed-by-haley-rane.square.site';
+  }
 }
